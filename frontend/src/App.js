b67f6bc5c33c69a1cfd7a5ee899556e5bfb052db
Rema ! <user@users-MacBook-Pro.local>
2025-10-12 16:25:25 +0300

    Fix frontend folder (remove submodule and add files correctly)

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
+import './styles/App.css';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<SignUp />} />
+    </Routes>
+  );
+}
+
+export default App;
